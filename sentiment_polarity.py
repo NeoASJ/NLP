869756c04ee2d83ee5b51849511cc0377989d411
Anush S Jathan <anushjathan268@gmail.com>
2025-06-24 21:13:50 +0530

    Delete sentiment_polarity.py

--- sentiment_polarity.py
+++
@@ -1,9 +0,0 @@
-from nltk.sentiment import SentimentIntensityAnalyzer
-import nltk
-
-nltk.download('vader_lexicon')
-
-sia = SentimentIntensityAnalyzer()
-sentence = "I love learning about NLP! It's fascinating."
-score = sia.polarity_scores(sentence)
-print(score)
